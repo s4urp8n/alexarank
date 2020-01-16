fe15b483897e479861cb6df80d27dd7d196f0938
Alexander Tarasov <s4urp8n@yandex.ru>
2020-01-16 22:34:10 +0300

    alexarank

---
+++ src/Zver/GoogleSafeBrowsing.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zver;
+
+class GoogleSafeBrowsing
+{
+    public static function getUnsafeSites(array $sites, $apiKey)
+    {
+        $url = "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=" . $apiKey;
+        $data = [
+            "threatInfo" => [
+                "threatTypes"      => [
+                    "MALWARE",
+                    "SOCIAL_ENGINEERING",
+                    "UNWANTED_SOFTWARE",
+                    "POTENTIALLY_HARMFUL_APPLICATION",
+                ],
+                "platformTypes"    => ["WINDOWS"],
+                "threatEntryTypes" => ["URL"],
+                "threatEntries"    => array_map(function ($site) {
+                    return ["url" => $site];
+                }, array_unique($sites)),
+            ],
+        ];
+
+        $matches = Curl::json($url, $data)->getContent();
+        if (!array_key_exists('matches', $matches)) {
+            return [];
+        }
+        return array_map(function ($match) {
+            return $match['threat']['url'];
+        }, $matches['matches']);
+    }
+}
--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -2,7 +2,20 @@
 
 class SampleTest extends PHPUnit\Framework\TestCase
 {
-    public function testSample()
+    public function testGoogle()
+    {
+        $sites = [
+            "veriprtroutg.com",
+            "notify-system.com",
+            "special-offers.online",
+            "tirsmile.pro",
+        ];
+        $expected = ["tirsmile.pro"];
+        $unsafe = \Zver\GoogleSafeBrowsing::getUnsafeSites($sites, '--secret--');
+        $this->assertEquals($unsafe, $expected);
+    }
+
+    public function testAlexarank()
     {
         $rank = \Zver\AlexaRank::getSiteGlobalRank('pornhub.com');
         $this->assertEquals(46, $rank);
